16e0447a03e4728727ee7ef977bf5bcc0db267f9
robbieculkin <robbieculkin@gmail.com>
2016-11-07 17:10:00 -0800

    cleaned_up is now master

--- master/master.ino
+++ master/master.ino
@@ -4,49 +4,69 @@
 
 #include <Adafruit_NeoPixel.h> //https://github.com/adafruit/Adafruit_NeoPixel
 
-//Pins
-#define LED_PIN 3
-#define LED_PIN2 9
-#define STROBE 4
-#define RESET 6
-#define DC_ONE A1
-#define DC_TWO A2
-#define POT A0
+// PINS & INFO
+#define STROBE           4
+#define RESET            6
+#define DC_ONE          A1
+#define DC_TWO          A2
+#define STRIP_PIN        3
+#define N_STRIP_LEDS   180
+#define TOWER_PIN        9
+#define N_TOWER_LEDS   180
+#define LEVELS          16
 
-//globals
-double oldVol = 0;
-int numLoops = 0;
-uint16_t j=0;
-uint32_t color [100];
-int frequencies[7];
-int oldFreqs[7];
-double vols[100];
-
-//settings
-#define TIME       8
-#define MULTIPLIER 90
-#define DIVISOR 1
-#define MAX_VOL    600
-#define UP         0.25      //(0 to 1) higher values mean livelier display
-#define DOWN       0.1   
-#define DIMMER     1.75
-#define CONTRAST   1.3
-#define LIVELINESS  2
+//SETTINGS         
+#define BRIGHTNESS  150       //(0 to 255)
+#define RISE_RATE   0.25      //(0 to 1) higher values mean livelier display
+#define FALL_RATE   0.10      //(0 to 1) higher values mean livelier display
+#define CONTRAST   1.3        //(undefined range)
+#define MAX_VOL    600        //(0 to 1023) maximum value reading expected from the shield
+#define LIVELINESS  2         //(undefined range)
 #define LIVELINESS2 1.7
+#define MULTIPLIER 90         //(undefined range) higher values mean fewer LEDs lit @ a given volume
 
-//tower info
-#define LEVELS 16
-//NeoPixel intitializations
-Adafruit_NeoPixel strip1 = Adafruit_NeoPixel(180, LED_PIN, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel strip2 = Adafruit_NeoPixel(120, LED_PIN2, NEO_GRB + NEO_KHZ800);
-//uint32_t color = strip1.Color(0, 0, 0);
+//NeoPixel intitialization
+Adafruit_NeoPixel strip1 = Adafruit_NeoPixel(N_STRIP_LEDS, STRIP_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel tower  = Adafruit_NeoPixel(N_TOWER_LEDS, TOWER_PIN, NEO_GRB + NEO_KHZ800);
+
+// READ
+    void recordFrequencies(int (&frequencies) [7]);
+    // records frequencies into the frequencies array
+    double getAvg(const int (&frequencies) [7]);
+    // returns avg of frequencies
+    
+//FILTER
+    double smoothVol(double newVol);
+    // brings vol values closer together, resulting a smoother output (avoid strobing)
+    double autoMap(const double vol);
+    // scales volume values in order to get consisitent output at different overall volumes
+
+//DISPLAY
+    void shiftColors();
+    // makes room for a new color value
+    uint32_t GetColor(byte pos, double vol);
+    // finds the new color value based on color rotation position and volume
+    void displayStrip(const double vol, Adafruit_NeoPixel &strip);
+    // detemines what LEDs are on/off; sends RGB values to the strip
+    void disiplayTower(const double vol, Adafruit_NeoPixel &strip);
+    // detemines what LEDs are on/off; sends RGB values to the strip
+
+//HELPER FUNCTIONS
+    int findLED(int level, int place);
+    //returns the index of any LED on the tower given its level and position on the level
 
 
-void setup() 
-{
-  Serial.begin(9600);
+//GLOBALS (globals are normally a bad idea, but the nature of Arduino's loop() 
+//         function makes them necessary)
+double vols[100];
+uint32_t color [100];
+uint32_t colorCursor = 0;
+int numLoops = 0;
+
+
+void setup() {
   
-//Spectrum Shield pin configurations
+  //Spectrum Shield pin configurations
   pinMode(STROBE, OUTPUT);
   pinMode(RESET, OUTPUT);
   pinMode(DC_ONE, INPUT);
@@ -66,134 +86,107 @@ void setup()
   digitalWrite(RESET, LOW);
 
   strip1.begin();
-  strip1.setBrightness(150);
+  strip1.setBrightness(BRIGHTNESS);
   strip1.show();
-  strip2.begin();
-  strip2.setBrightness(150);
-  strip2.show();
+  tower.begin();
+  tower.setBrightness(BRIGHTNESS);
+  tower.show();
+
 }
 
-void loop() 
-{
+void loop() {
+  int frequencies[7];
+  double volume;
+  static uint16_t pos = 0;
   
-  Read_Frequencies();
-  double vol = GetAvgVolume(); 
-  vol = SmoothVols(vol); // smooths volumes before they go to the array
-  double newVol = AutoMap(vol);
-  double TowerVol = AutoMap(vol);
-  ShiftArrayRight();
-  color[0] = GetColor(j & 255, vol/DIMMER);
-  Display_FTM(newVol, strip1);
-  SmoothFreqs();
-  VerticalColorShift(TowerVol, strip2);
-  //Display_FTM(newVol, strip2);
-  delay(TIME); // slows pulses based on volume
-  //Serial.println(newVol);
-  int roc = map (newVol, 0, strip1.numPixels()/2.0, 0, 25);
-  
-  oldVol = vol;
+  //
+  // READ
+  //
+  recordFrequencies(frequencies);
+  volume = getAvg(frequencies);
+
+  //
+  // FILTER
+  //
+  volume = smoothVol(volume);
+  volume = autoMap(volume);
+
+  //
+  // ASSIGN COLOR/BRIGHTNESS
+  //
+  shiftColors();
+  color[colorCursor] = GetColor(pos & 255, volume);
+
+  //rotate the color assignment wheel
+  int rotation = map (volume, 0, strip1.numPixels()/2.0, 0, 25);
   numLoops++;
-  if(numLoops%5 == 0) //default 1
-  {
-    j+=roc; // increase to rotate colors faster. default 2
-  }
-}
+  if(numLoops%5 == 0) //default 5
+    pos+=rotation;
 
-void Read_Frequencies() //Read frequencies for each band
-{ 
-  int band;
-  for (band = 0; band<7; band++)
-  {                                         // + analogRead(DC_TWO)
-    frequencies[band] = ((analogRead(DC_ONE)));
-    // ^ take avg of the two sensors and divide by 2 using a left shift 
-    if (frequencies[band] < 70) frequencies[band] = 0;
-    digitalWrite(STROBE, HIGH);
-    digitalWrite(STROBE, LOW);
-//    Serial.print(band);
-//    Serial.print(": ");
-//    Serial.println(frequencies[band]);
-  }
-}
-void Display_All(double vol, Adafruit_NeoPixel &strip)
-{
-  vol = pow(vol, 1.75);
+  //
+  // DISPLAY
+  //  
+  displayStrip(volume, strip1);
+  disiplayTower(volume, tower);
   
-  int i;
-  for (i = 0; i < strip.numPixels()/2; i++)
-  {
-    double threshold = ((double)(i+1)*MULTIPLIER)/DIVISOR-3*vol+5;
-    
-    strip.setPixelColor(strip.numPixels()/2-1-i, color[i]);
-
-    strip.setPixelColor(strip.numPixels()/2+i, color[i]);
-  } 
-  strip.show();
 }
 
-void Display_FTM (double vol, Adafruit_NeoPixel &strip)
+void recordFrequencies(int (&frequencies) [7])
 {
-  uint32_t off = strip1.Color(0, 0, 0);
-
-  vol = 0.5*vol;
-  vol = pow(vol, LIVELINESS);
-  
-  int i;
-  for (i = 0; i < strip.numPixels(); i++)
-  {
-    double threshold = ((double)(i+1)*MULTIPLIER)/DIVISOR-3*vol+5;
-    if (vol > threshold)
+    int band;
+    for (band = 0; band<7; band++)
     {
-      strip.setPixelColor(strip.numPixels()/2-1-i, color[i]);
+        frequencies[band] = ((analogRead(DC_ONE) + analogRead(DC_TWO))/2);
+        
+        if (frequencies[band] < 70) frequencies[band] = 0;
+        
+        //account for noise
+        digitalWrite(STROBE, HIGH);
+        digitalWrite(STROBE, LOW);
     }
-    else
-    {
-      strip.setPixelColor(strip.numPixels()/2-1-i, off);
-    }
-    
-    if (vol > threshold)
-    {
-      strip.setPixelColor(strip.numPixels()/2-1+i, color[i]);
-    }
-    else
-    {
-      strip.setPixelColor(strip.numPixels()/2-1+i, off);
-    }
-  }
-  strip.show();
 }
 
-double GetAvgVolume() //takes the average of NUM_AVG readings from the sensor
+double getAvg(const int (&frequencies)[7])
 {
   int total = 0;
-  int k;
-  for(k=0; k < 7; k++)
+  
+  for(int k=0; k < 7; k++)
   {
     total+= frequencies[k];
   }
+  
   return ((double)total/7);
 }
 
-double SmoothVols(double newVol)
+double smoothVol(double newVol)
 {
+  double oldVol = vols[0];
+  
   if (oldVol < newVol)
-      return (newVol * (UP)) + (oldVol * (1-(UP)));
+  {
+      newVol = (newVol * RISE_RATE) + (oldVol * (1-RISE_RATE));
+      // limit how quickly volume can rise from the last value
+      
+      return newVol;
+  }
+      
   else
-      return (newVol * (DOWN)) + (oldVol * (1-(DOWN)));
+  {
+      newVol = (newVol * FALL_RATE) + (oldVol * (1-FALL_RATE));
+      // limit how quickly volume can fall from the last value
+      
+      return newVol;
+  }
+      
 }
 
-double ManualMap(double vol)
-{
-  int potVal = analogRead(POT);
-
-  return vol*potVal/1024;
-}
-
-double AutoMap(double vol)
+double autoMap(const double vol)
 {
   double total = 0;
   static double avg;
   static int prevMillis = 0;
-  
+
+  // this if statement acts like a delay, but without putting the entire sketch on hold
   if(millis() - prevMillis > 100)
   {
     for (int i = 100; i > 0; i--) 
@@ -202,8 +195,8 @@ double AutoMap(double vol)
       vols[i] = vols[i-1]; 
     }
     vols[0] = vol;
+    
     avg = total/100.0;
-    //if (avg < 10) avg = 10;
     
     prevMillis = millis();
   }
@@ -212,9 +205,7 @@ double AutoMap(double vol)
   return map (vol, 0, 3*avg+1, 0, strip1.numPixels()/1.0);
 }
 
-//TODO eliminate this function using a position variable
-//OR use a deque to store colors to improve efficiency
-void ShiftArrayRight()
+void shiftColors()
 {
   for (int i = 99; i >=1; i--) 
   {
@@ -225,6 +216,8 @@ void ShiftArrayRight()
 uint32_t GetColor(byte pos, double vol) //returns color & brightness
 {
   pos = 255 - pos;
+
+  // some last-minute transforms I dont want to stick in the main fcn
   vol = pow(vol, CONTRAST);
   vol = map(vol, 0, MAX_VOL, 0, 255);
 
@@ -249,54 +242,54 @@ if(vol >255) vol = 255;
   int blu = 0;
   return strip1.Color(vol*red/255, vol*grn/255, vol*blu/255);
 }
-void SmoothFreqs()
-{
-  int i;
-  for (i=0; i<7; i++)
-  {
-    if (oldFreqs[i] < frequencies[i])
-        frequencies[i] = (frequencies[i] * (0.1)) + (oldFreqs[i] * (1-(0.1)));
-    else
-        frequencies[i] = (frequencies[i] * (0.1)) + (oldFreqs[i] * (1-(0.1)));
-  }
-}
 
-int findLED(int level, int index)
-{
-  int i, total = 0;
-  for(i=0; i < level; i++)
-  {
-    if(i%2 == 0)
-    {
-      total += 5;
-    }
-    else
-    {
-      total += 10;
-    }
-  }
-  if (level%2 == 0 && index > 4)
-  {
-    index = 4;
-  }
-  total += index;
-  //Serial.println(total);
-  return total;
-}
-
-
-void VerticalColorShift(double vol, Adafruit_NeoPixel &strip)
+void displayStrip(const double vol, Adafruit_NeoPixel &strip)
 {
   uint32_t off = strip1.Color(0, 0, 0);
 
-  vol = 0.4*vol;
-  vol = pow(vol, LIVELINESS2);
+  // some last-minute transforms I dont want to stick in the main fcn
+  double newVol = 0.5*vol;
+  newVol = pow(newVol, LIVELINESS);
+  
+  int i;
+  for (i = 0; i < strip.numPixels(); i++)
+  {
+    // some magic numbers here that made the output better. should eventually get rid of them
+    double threshold = ((double)(i+1) * MULTIPLIER - (3*newVol)+5);
+     
+    if (newVol > threshold)
+    {
+      strip.setPixelColor(strip.numPixels()/2-1-i, color[i]);
+    }
+    else
+    {
+      strip.setPixelColor(strip.numPixels()/2-1-i, off);
+    }
+    
+    if (newVol > threshold)
+    {
+      strip.setPixelColor(strip.numPixels()/2-1+i, color[i]);
+    }
+    else
+    {
+      strip.setPixelColor(strip.numPixels()/2-1+i, off);
+    }
+  }
+  strip.show();
+}
+
+void disiplayTower(const double vol, Adafruit_NeoPixel &strip)
+{
+  uint32_t off = strip1.Color(0, 0, 0);
+
+  double myVol = 0.4*vol;
+  myVol = pow(myVol, LIVELINESS2);
   
   int i, j;
   for (i=0; i < LEVELS; i+=2)
   {
-    double threshold = (((double)(i+1)*MULTIPLIER)/DIVISOR-3*vol+5)*14;
-    if(vol > threshold)
+    double threshold = (((double)(i+1)*MULTIPLIER)-3*myVol+5)*14;
+    if(myVol > threshold)
     {
       for (j=0; j < 5; j++)
       {
@@ -313,7 +306,7 @@ void VerticalColorShift(double vol, Adafruit_NeoPixel &strip)
   }
   for (i=1; i < LEVELS; i+=2)
   {
-    double threshold = (((double)(i+1)*MULTIPLIER)/DIVISOR-3*vol+5)*14;
+    double threshold = (((double)(i+1)*MULTIPLIER)-3*myVol+5)*14;
     if(vol > threshold)
     {
       for (j=0; j < 10; j++)
@@ -330,4 +323,27 @@ void VerticalColorShift(double vol, Adafruit_NeoPixel &strip)
     }
   }
   strip.show();
+}
+
+int findLED(int level, int place)
+{
+  int i, total = 0;
+  for(i=0; i < level; i++)
+  {
+    if(i%2 == 0)
+    {
+      total += 5;
+    }
+    else
+    {
+      total += 10;
+    }
+  }
+  if (level%2 == 0 && place > 4)
+  {
+    place = 4;
+  }
+  total += place;
+
+  return total;
 }
