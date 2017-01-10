51856d11e7ebb193fc7d15b2f597837aba07ef90
robbieculkin <robbieculkin@gmail.com>
2017-01-09 17:37:31 -0800

    Revert "ChangedBluetoothInput"
    This reverts commit 466acc860bfbcc3216fd5a4b1b0ec015724a02ab.

--- master/master.ino
+++ master/master.ino
@@ -2,9 +2,12 @@
 //Robbie Culkin & Daniel Barkhorn
 //for the project found at https://robbieculkin.wordpress.com/2016/08/02/rgb-led-audio-visualizer/
 
+#include <SoftwareSerial.h>
 #include <Adafruit_NeoPixel.h> //https://github.com/adafruit/Adafruit_NeoPixel
 
 // PINS & INFO
+#define BT_TX           A9
+#define BT_RX           A8
 #define STROBE           4
 #define RESET            6
 #define DC_ONE          A1
@@ -17,17 +20,18 @@
 
 //SETTINGS
 #define BRIGHTNESS  150       //(0 to 255)
-#define RISE_RATE     0.25    //(0 to 1) higher values mean livelier display
-#define FALL_RATE     0.10    //(0 to 1) higher values mean livelier display
+#define RISE_RATE     0.15    //(0 to 1) higher values mean livelier display
+#define FALL_RATE     0.05    //(0 to 1) higher values mean livelier display
 #define CONTRAST      1.3     //(undefined range)
 #define MAX_VOL     600       //(0 to 1023) maximum value reading expected from the shield
 #define LIVELINESS    2       //(undefined range)
 #define LIVELINESS_T  1.7     //(undefined range)
 #define MULTIPLIER   90       //(undefined range) higher values mean fewer LEDs lit @ a given volume
 
-//NeoPixel intitialization
+//NeoPixel & Bluetooth intitialization
 Adafruit_NeoPixel strip1 = Adafruit_NeoPixel(N_STRIP_LEDS, STRIP_PIN, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel tower  = Adafruit_NeoPixel(N_TOWER_LEDS, TOWER_PIN, NEO_GRB + NEO_KHZ800);
+SoftwareSerial BT_Board(BT_TX, BT_RX);    //TX, RX
 
 // READ
 void recordFrequencies(int (&frequencies) [7]);
@@ -77,6 +81,7 @@ void setup() {
   }
 
   Serial.begin(9600);
+  BT_Board.begin(9600);
 
   //Spectrum Shield pin configurations
   pinMode(STROBE, OUTPUT);
@@ -110,6 +115,10 @@ void loop() {
   double volume;
   static uint16_t pos = 0;
 
+
+  if (BT_Board.available())
+    Serial.write(BT_Board.read());
+    
   //
   // READ
   //
@@ -125,7 +134,7 @@ void loop() {
   //
   // DISPLAY
   //
-  shiftColors();
+  shiftColors(); // TODO: circular array. Shifts inefficient
   color[0] = GetColor(pos & 255, volume);
 
   //rotate the color assignment wheel
@@ -371,24 +380,22 @@ void displayTower(const double vol, Adafruit_NeoPixel &strip)
   {
     if(s[0] == 'C')   // Changing contrast value
     {
-      redMult*=(int)s.substring(1);
-      greenMult*=(int)s.substring(1);
-      blueMult*=(int)s.substring(1);
+      // Change contrast
       return;
     }
     if(s[0] == 'R')  
     {
-      redMult = (int)s.substring(1);  
+      // Change red intensity
       return;
     }
     if(s[0] == 'G')
     {
-      greenMult = (int)s.substring(1);
+      // Change green intensity
       return;
     }
     if(s[0] == 'B')
     {
-      blueMult = (int)s.substring(1);
+      // Change blue intensity
       return;
     }
   }
